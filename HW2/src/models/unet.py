6869251badf7565b384b79fc3a62a9a8eaac3bee
bill901230 <your_email@example.com>
2025-03-16 23:52:20 +0800

    implement unet

--- HW2/src/models/unet.py
+++ HW2/src/models/unet.py
@@ -1,3 +1,56 @@
-# Implement your UNet model here
+import torch
+import torch.nn as nn
 
-assert False, "Not implemented yet!"
+class ConvBlock(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+class UNet(nn.Module):
+    def __init__(self, in_channels=3, out_channels=1):
+        super().__init__()
+
+        self.pool = nn.MaxPool2d(2)
+
+        self.enc1 = ConvBlock(in_channels, 64)
+        self.enc2 = ConvBlock(64, 128)
+        self.enc3 = ConvBlock(128, 256)
+        self.enc4 = ConvBlock(256, 512)
+
+        self.bottleneck = ConvBlock(512, 1024)
+
+        self.up4 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.dec4 = ConvBlock(1024, 512)
+        self.up3 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.dec3 = ConvBlock(512, 256)
+        self.up2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.dec2 = ConvBlock(256, 128)
+        self.up1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.dec1 = ConvBlock(128, 64)
+
+        self.final_conv = nn.Conv2d(64, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        enc1 = self.enc1(x)
+        enc2 = self.enc2(self.pool(enc1))
+        enc3 = self.enc3(self.pool(enc2))
+        enc4 = self.enc4(self.pool(enc3))
+
+        bottleneck = self.bottleneck(self.pool(enc4))
+
+        dec4 = self.dec4(torch.cat((enc4, self.up4(bottleneck)), dim=1))
+        dec3 = self.dec3(torch.cat((enc3, self.up3(dec4)), dim=1))
+        dec2 = self.dec2(torch.cat((enc2, self.up2(dec3)), dim=1))
+        dec1 = self.dec1(torch.cat((enc1, self.up1(dec2)), dim=1))
+
+        return self.final_conv(dec1)
