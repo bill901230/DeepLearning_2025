a021ffe795480a4959b5a9aa72df80a9e3d3060a
bill901230 <your_email@example.com>
2025-03-18 19:07:54 +0800

    try to download dataset

--- HW2/src/train.py
+++ HW2/src/train.py
@@ -1,9 +1,22 @@
 import argparse
+import os
+from oxford_pet import SimpleOxfordPetDataset, load_dataset
+from models.unet import UNet
+from models.resnet34_unet import ResNet34_UNet
 
 def train(args):
     # implement the training function here
+    dataset_root = args.data_path
+    if not os.path.exists(os.path.join(dataset_root, "images")):
+        print("Dataset not found. Downloading now...")
+        SimpleOxfordPetDataset.download(dataset_root)
+
+    train_loader = load_dataset(data_path=dataset_root, mode="train", batch_size=args.batch_size)
+    valid_loader = load_dataset(data_path=dataset_root, mode="valid", batch_size=args.batch_size, shuffle=False)
+
+    # model = UNet()
+    # model = ResNet34_UNet()
     
-    assert False, "Not implemented yet!"
 
 def get_args():
     parser = argparse.ArgumentParser(description='Train the UNet on images and target masks')
@@ -16,3 +29,4 @@ def get_args():
  
 if __name__ == "__main__":
     args = get_args()
+    train(args)
